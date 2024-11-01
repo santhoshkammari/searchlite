6c59779fbd2bfdd7eda4ecf5bd729de0c0e00cb7
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-11-01 19:22:14 +0530

    v0.6.0

--- src/searchlite/providers.py
+++ src/searchlite/providers.py
@@ -1,21 +1,25 @@
 from .searcher import RealTimeGoogleSearchProvider
 
-def google(query,max_urls=5):
-    search = RealTimeGoogleSearchProvider()
+def google(query,max_urls=5,animation=False):
+    search = RealTimeGoogleSearchProvider(animation=animation)
     return search.search(query,max_urls=max_urls)
 
-def bing(query,max_urls=5):
-    search = RealTimeGoogleSearchProvider(search_provider="bing")
+def bing(query,max_urls=5,animation=False):
+    search = RealTimeGoogleSearchProvider(search_provider="bing",
+                                          animation=animation)
     return search.search(query, max_urls=max_urls)
 
-def duckduckgo(query,max_urls=5):
-    search = RealTimeGoogleSearchProvider(search_provider="duckduckgo")
+def duckduckgo(query,max_urls=5,animation=False):
+    search = RealTimeGoogleSearchProvider(search_provider="duckduckgo",
+                                          animation=animation)
     return search.search(query, max_urls=max_urls)
 
-def yahoo(query,max_urls=5):
-    search = RealTimeGoogleSearchProvider(search_provider="yahoo")
+def yahoo(query,max_urls=5,animation=False):
+    search = RealTimeGoogleSearchProvider(search_provider="yahoo",
+                                          animation=animation)
     return search.search(query, max_urls=max_urls)
 
-def ask(query,max_urls=5):
-    search = RealTimeGoogleSearchProvider(search_provider="ask")
+def ask(query,max_urls=5,animation=False):
+    search = RealTimeGoogleSearchProvider(search_provider="ask",
+                                          animation=animation)
     return search.search(query, max_urls=max_urls)
